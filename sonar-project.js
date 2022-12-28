a8570dc13758dac9d985d4687f1fa3f337976342
Basit Hussain <bassithussain98@gmail.com>
2022-12-28 17:07:26 +0500

    sonarqibe configurations

---
+++ sonar-project.js
@@ -0,0 +1,17 @@
+const sonarqubeScanner = require("sonarqube-scanner");
+sonarqubeScanner(
+  {
+    serverUrl: "http://localhost:9000",
+    token: "YOUR-TOKEN-HERE",
+    options: {
+      "sonar.sources": "./src",
+      "sonar.exclusions": "**/__tests__/**",
+      "sonar.tests": "./src/__tests__",
+      "sonar.test.inclusions":
+        "./src/__tests__/**/*.test.tsx,./src/__tests__/**/*.test.ts",
+      "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",
+      "sonar.testExecutionReportPaths": "reports/test-report.xml",
+    },
+  },
+  () => {}
+);
